d731aa109361c2809d3f800299a6ccb22229f00c
Sergey Batukov <sashachepuha@gmail.com>
2022-01-27 12:36:58 +0000

    add scenes

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import c from './services/logger.servise.js'
+import scenes from './services/scenes.service.js'
+import inquirer from 'inquirer'
 
-c.info(123)
-c.sucsess(123)
-c.warning(123)
-c.error(123)
-c.obj('OBJ', {
-  a: 123,
-  b: '123'
-})
+// log service
+// c.info(123)
+// c.sucsess(123)
+// c.warning(123)
+// c.error(123)
+// c.obj('OBJ', {
+//   a: 123,
+//   b: '123'
+// })
 
 // food -c  СОЗДАНИЕ ИЛИ ПЕРЕЗАПИСЬ КОНФИГА
 // Ввод роста
@@ -15,3 +18,20 @@ c.obj('OBJ', {
 // Ввод возраста
 // Расчет необходимых вешеств
 // Запись конфига
+
+async function scene(array) {
+  const result = {}
+  for (const q of array) {
+    await inquirer.prompt(q).then(answer => {
+      result[q.name] = answer[q.name]
+    })
+  }
+  return result
+}
+
+async function init() {
+  const config = await scene(scenes.config)
+  c.obj('CONFIG', config)
+}
+
+init()
---
+++ services/scenes.service.js
@@ -0,0 +1,19 @@
+export default {
+  config: [
+    {
+      type: "number",
+      name: "age",
+      message: "Ваш возраст:",
+    },
+    {
+      type: "number",
+      name: "growth",
+      message: "Ваш рост:",
+    },
+    {
+      type: "number",
+      name: "weight",
+      message: "Ваш вес:",
+    }
+  ]
+}
