4ddf965e0a612260c3d9c774775fd52ea6cb5e9c
Sergey Batukov <sashachepuha@gmail.com>
2022-01-27 12:56:46 +0000

    remove commits

--- index.js
+++ index.js
@@ -4,24 +4,6 @@ import c from './services/logger.servise.js'
 import scene from './services/scenes.service.js'
 import commander from 'commander'
 
-
-// log service
-// c.info(123)
-// c.sucsess(123)
-// c.warning(123)
-// c.error(123)
-// c.obj('OBJ', {
-//   a: 123,
-//   b: '123'
-// })
-
-// food c  СОЗДАНИЕ ИЛИ ПЕРЕЗАПИСЬ КОНФИГА
-// Ввод роста
-// Ввод веса
-// Ввод возраста
-// Расчет необходимых вешеств
-// Запись конфига
-
 commander
   .command('config')
   .alias('c')
